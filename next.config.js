76133cac4eb24f0f292338ecdddd52f6d719d1dd
iisuslik9 <150892541+iisuslik9@users.noreply.github.com>
2025-12-16 23:14:56 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  experimental: {
+    esmExternals: 'loose'
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fullySpecified = false
+    }
     config.module.rules.push({
       test: /\.mjs$/,
       include: /node_modules/,
